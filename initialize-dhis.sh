61f304a751a4b33dc6ceda614522578a01903b8e
joas japhes <joasjaphes665@gmail.com>
2021-06-13 09:13:42 +0300

    modified

--- initialize-dhis.sh
+++ initialize-dhis.sh
@@ -1,5 +1,5 @@
 docker-compose down
 docker-compose up --build -d
-docker exec jostano-db psql -U dhis -c "DROP DATABASE jostano"
-docker exec jostano-db psql -U dhis -c "CREATE DATABASE jostano"
+docker exec jostano-db psql -U dhis postgres -c "DROP DATABASE jostano"
+docker exec jostano-db psql -U dhis postgres -c "CREATE DATABASE jostano"
 docker container start jostano-dhis
--- insert-data.sh
+++ insert-data.sh
@@ -1,6 +1,7 @@
 docker container stop jostano-dhis
-docker exec -it jostano-db bash
-cd db-backups
-psql -U dhis jostano < initial_db.sql
+docker exec jostano-db psql -U dhis jostano < /db-backups/initial_db.sql
+# docker exec -it jostano-db bash
+# cd db-backups
+# psql -U dhis jostano < initial_db.sql
 docker container start jostano-dhis
 exit
